3117e81a01cdc3af6a0d11d8815cbc0db6fe38b3
Kelly Diabagate <diabagatekelly@yahoo.fr>
2019-01-30 18:01:11 -0600

    Final commit

---
+++ koans/2-AboutFunctions.js
@@ -0,0 +1,128 @@
+describe("2. About Functions", () => {
+  it("should declare functions", () => {
+    const add = (a, b) => {
+      return a + b;
+    }
+
+    expect(add(1, 2)).toBe(3);
+  });
+
+  it("should know internal variables override outer variables", () => {
+    const message = "Outer";
+
+    const getMessage = () => {
+      return message;
+    }
+
+    const overrideMessage = () => {
+      const message = "Inner";
+      return message;
+    }
+
+    expect(getMessage()).toBe("Outer");
+    expect(overrideMessage()).toBe("Inner");
+    expect(message).toBe("Outer");
+  });
+
+  it("should have lexical scoping", () => {
+    const variable = "top-level";
+    const parentfunction = () => {
+      const variable = "local";
+      const childfunction = () => {
+        return variable;
+      }
+      return childfunction();
+    }
+    expect(parentfunction()).toBe("local");
+  });
+
+  it("should use lexical scoping to synthesise functions", () => {
+    makeMysteryFunction = makerValue => {
+      const newFunction = function doMysteriousThing(param)
+      {
+        return makerValue + param;
+      };
+      return newFunction;
+    }
+
+    const mysteryFunction3 = makeMysteryFunction(3);
+    const mysteryFunction5 = makeMysteryFunction(5);
+
+    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(23);
+  });
+
+  it("should allow extra function arguments", () => {
+    const returnFirstArg = firstArg => {
+      return firstArg;
+    }
+
+    expect(returnFirstArg("first", "second", "third")).toBe("first");
+
+    const returnSecondArg = (firstArg, secondArg) => {
+      return secondArg;
+    }
+
+    expect(returnSecondArg("only give first arg")).toBe(undefined);
+
+    const returnAllArgs = (...arguments) => {
+      let argsArray = [];
+      for (let i = 0; i < arguments.length; i+= 1) {
+        argsArray.push(arguments[i]);
+      }
+      return argsArray.join(",");
+    }
+
+    expect(returnAllArgs("first", "second", "third")).toBe('first,second,third');
+  });
+
+
+  it("should return undefined if no return value is specified", () => {
+    const returnsUndefined = () => {
+
+    }
+
+    expect(returnsUndefined()).toBe(undefined);
+  });
+
+  it("should pass functions as values", () => {
+    const appendRules = name => {
+      return name + " rules!";
+    };
+
+    const appendDoubleRules = name => {
+      return name + " totally rules!";
+    };
+
+    const praiseSinger = { givePraise: appendRules };
+    expect(praiseSinger.givePraise("John")).toBe("John rules!");
+
+    praiseSinger.givePraise = appendDoubleRules;
+    expect(praiseSinger.givePraise("Mary")).toBe("Mary totally rules!");
+  });
+
+  it("can use a function that returns a function", () => {
+    const myFunc = () => {
+      let count = 0;
+      return () => {
+        return count + 1;
+      };
+    };
+
+    expect(myFunc()()).toEqual(1);
+    expect( typeof myFunc() ).toEqual("function");
+  });
+
+  it("can use functions and closures", () =>{
+    const myFunc = () => {
+      let count = 0;
+      return () => {
+        count = count + 1;
+        return count;
+      };
+    };
+
+    const closure = myFunc();
+    expect(closure()).toEqual(1);
+    expect(closure()).toEqual(2);
+  });
+});
---
+++ koans/3-AboutObjects.js
@@ -0,0 +1,111 @@
+describe("3. About Objects", () => {
+
+  describe("Properties", () => {
+    let megalomaniac;
+
+    beforeEach(() => {
+      megalomaniac = {  mastermind: "Joker", henchwoman: "Harley" };
+    });
+
+    it("should confirm objects are collections of properties", () => {
+      expect(megalomaniac.mastermind).toBe("Joker");
+    });
+
+    it("should confirm that properties are case sensitive", () => {
+      expect(megalomaniac.henchwoman).toBe("Harley");
+      expect(megalomaniac.henchWoman).toBe(undefined);
+    });
+  });
+
+
+  it("should know properties that are functions act like methods", () => {
+    const megalomaniac = {
+      mastermind : "Brain",
+      henchman: "Pinky",
+      battleCry: function (noOfBrains) {
+        return "They are " + this.henchman + " and the" +
+          Array(noOfBrains + 1).join(" " + this.mastermind);
+      }
+    };
+
+    const battleCry = megalomaniac.battleCry(4);
+    expect(megalomaniac.battleCry(4)).toMatch(battleCry);
+  });
+
+  it("should confirm that when a function is attached to an object, 'this' refers to the object", () => {
+    const currentDate = new Date();
+    const currentYear = (currentDate.getFullYear());
+    const megalomaniac = {
+      mastermind: "James Wood",
+      henchman: "Adam West",
+      birthYear: 1970,
+      calculateAge: function() {
+        return currentYear - this.birthYear;
+      }
+    };
+
+    expect(currentYear).toBe(2019);
+    expect(megalomaniac.calculateAge()).toBe(49);
+  });
+
+
+  it("should know that properties can be added and deleted", () => {
+    const megalomaniac = { mastermind : "Agent Smith", henchman: "Agent Smith" };
+
+    expect(megalomaniac["secretary"]).toBe(undefined);
+
+    megalomaniac.secretary = "Agent Smith";
+    expect(megalomaniac["secretary"]).toBe("Agent Smith");
+
+    delete megalomaniac.henchman;
+    expect(megalomaniac["henchman"]).toBe(undefined);
+  });
+
+
+  it("should use prototype to add to all objects", () => {
+    function Circle(radius){
+      this.radius = radius;
+    }
+
+    const simpleCircle = new Circle(10);
+    let colouredCircle = new Circle(5);
+    colouredCircle.colour = "red";
+
+    expect(simpleCircle.colour).toBe(undefined);
+    expect(colouredCircle.colour).toBe("red");
+
+    Circle.prototype.describe = () => {
+      return "This circle has a radius of: " + this.radius;
+    };
+
+    expect(simpleCircle.describe()).toBe("This circle has a radius of: " + undefined);
+    expect(colouredCircle.describe()).toBe("This circle has a radius of: " + undefined);
+  });
+
+  it("should create your own simple object", () => {
+    const myNewObject = {
+      prop1 : "a property!",
+      prop2: [4,3,2],
+      prop3 : () => {
+        return "a property that is a method!"
+      }
+    };
+
+    expect(myNewObject.prop1).toEqual("a property!");
+    expect(myNewObject.prop2).toEqual([4,3,2]);
+    expect(myNewObject.prop3()).toEqual("a property that is a method!");
+  });
+
+  it("should create your own prototype", () => {
+    function MyPrototype(argument) {
+      return {
+        prop2: argument,
+        prop1: {simple: "object" }
+      };
+    }
+    const myNewObject = new MyPrototype("initial value");
+
+    expect(myNewObject.prop1).toEqual({simple: "object"});
+    expect(myNewObject.prop2).toEqual("initial value");
+  });
+});
