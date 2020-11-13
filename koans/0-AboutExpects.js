6f874226304628966c4b18dd2dad0ae1f737caca
Kelly Diabagate <diabagatekelly@yahoo.fr>
2020-11-13 15:27:57 -0600

    final commit

--- koans/0-AboutExpects.js
+++ koans/0-AboutExpects.js
@@ -3,7 +3,7 @@ describe('0. About Expects', () => {
   // We shall contemplate truth by testing reality, via spec expectations.
   it('should expect true', () => {
 
-    // Your journey begins here: Replace the word false with true test
+    // Your journey begins here: Replace the word false with true
     expect(true).toBeTruthy();
   });
 
